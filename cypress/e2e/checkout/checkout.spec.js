f85b3b95b3c6c6bd054ce45a8a62b8ceb9028826
Gustavo Barros <gustavo.debarros@toptal.com>
2024-10-26 18:54:46 -0300

    refactors checkout test and adds more test covarage to the flow

--- cypress/e2e/checkout/checkout.spec.js
+++ cypress/e2e/checkout/checkout.spec.js
@@ -1,13 +1,12 @@
 /// <reference types="cypress" />;
 
 describe("Checkout", () => {
-  //Sobre as chaves: Não é o ideal, mas como esse projeto é uma avaliação, o arquivo .env não será incluso no arquivo .gitignore para comodidade dos avaliadores
+  //Sobre as chaves: Não é o ideal não adicionar o .env ao .gitignore, mas como esse projeto é uma avaliação, o arquivo ficara disponível para a comodidade dos avaliadores
   const username = Cypress.env("username")
   const password = Cypress.env("password")
 
-  it("checkout works fine", () => {
+  beforeEach(() => {
     cy.visit("/")
-
     cy.login(username, password)
 
     cy.addItem("Sauce Labs Backpack")
@@ -15,10 +14,10 @@ describe("Checkout", () => {
     cy.addItem("Sauce Labs Onesie")
 
     cy.get(".shopping_cart_link").click()
-    cy.get(".inventory_item_name").should("have.length", 3)
-
     cy.get("#checkout").click()
+  })
 
+  it("Validates checkout", () => {
     cy.get("#first-name").type("Nome")
     cy.get("#last-name").type("Sobrenome")
     cy.get("#postal-code").type("88101-707")
@@ -30,5 +29,76 @@ describe("Checkout", () => {
       "contain",
       "Thank you for your order!"
     )
+  })
+
+  it("Validates if First Name field is required", () => {
+    cy.get("#continue").click()
+    cy.contains("Error: First Name is required").should("be.visible")
+  })
+
+  it("Validates if Last Name field is required", () => {
+    cy.get("#first-name").type("Nome")
+    cy.get("#continue").click()
+    cy.contains("Error: Last Name is required").should("be.visible")
+  })
+
+  it("Validates if Zip/Postal Code field is required", () => {
+    cy.get("#first-name").type("Nome")
+    cy.get("#last-name").type("Sobrenome")
+    cy.get("#continue").click()
+    cy.contains("Error: Postal Code is required").should("be.visible")
+  })
+
+  it("Validates Your Information page elements", () => {
+    cy.get("#first-name")
+      .should("be.visible")
+      .and("have.attr", "placeholder", "First Name")
+
+    cy.get("#last-name")
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Last Name")
+    cy.get("#postal-code")
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Zip/Postal Code")
+
+    cy.get('[data-test="cancel"]')
+      .should("be.visible")
+      .and("have.text", "Cancel")
+
+    cy.get('[data-test="continue"]')
+      .should("be.visible")
+      .and("have.value", "Continue")
+  })
+
+  it("Validates Overview page elements", () => {
+    cy.fillsYourInformationPageData("Nome", "Sobrenome", "88101-070")
+
+    cy.get(".cart_item")
+      .first()
+      .within(() => {
+        cy.get(".inventory_item_name").should("be.visible")
+        cy.get(".inventory_item_desc").should("be.visible")
+        cy.get(".inventory_item_price").should("be.visible")
+        cy.get(".cart_quantity").should("be.visible")
+      })
+    cy.get(".summary_info_label").should("be.visible")
+    cy.get(".summary_value_label").should("be.visible")
+    cy.get(".summary_subtotal_label").should("be.visible")
+    cy.get(".summary_tax_label").should("be.visible")
+    cy.get(".summary_total_label").should("be.visible")
+  })
+
+  it("Cancels checkout on the Your Information page", () => {
+    cy.get('[data-test="cancel"]').click()
+
+    cy.url().should("include", "/cart.html")
+  })
+
+  it("Cancels checkout on the Overview page", () => {
+    cy.fillsYourInformationPageData("Nome", "Sobrenome", "88101-070")
+
+    cy.get('[data-test="cancel"]').click()
+
+    cy.url().should("include", "/inventory.html")
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,13 @@ Cypress.Commands.add("login", (username, password) => {
 Cypress.Commands.add("addItem", (itemName) => {
   cy.contains(".inventory_item", itemName).contains("Add to cart").click()
 })
+
+Cypress.Commands.add(
+  "fillsYourInformationPageData",
+  (firstName, lastName, zipCode) => {
+    cy.get("#first-name").type(firstName)
+    cy.get("#last-name").type(lastName)
+    cy.get("#postal-code").type(zipCode)
+    cy.get("#continue").click()
+  }
+)
